3ea4dbf35c8369945cc14543f9ba18180ea74beb
jiang <jianglinair@163.com>
2021-02-02 16:24:32 +0800

    moda

--- libbusa/src/main/java/com/jianglin/libbusa/BusaActivity.java
+++ libbusa/src/main/java/com/jianglin/libbusa/BusaActivity.java
@@ -1,12 +1,15 @@
 package com.jianglin.libbusa;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.jianglin.libcommon.BaseActivity;
 import com.jianglin.libexpa.BusaPath;
+import com.jianglin.libexpb.BUtil;
 import com.jianglin.libexpb.BusbPath;
 
 @Route(path = BusaPath.BUSA)
@@ -17,6 +20,12 @@ public class BusaActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_busa);
+
+        try {
+            ((TextView) findViewById(R.id.textView2)).setText(BUtil.getInfo().info);
+        } catch (Exception e) {
+            Log.e(TAG, "[NAIPIQ]onCreate: 获取组件B的服务出错", e);
+        }
     }
 
     @Override
